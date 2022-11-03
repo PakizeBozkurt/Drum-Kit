1624de36b78a81ad0b0e3fc97a78d5875e525c0d
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-03 21:49:02 +0000

    Added addEventListener

---
+++ index.js
@@ -0,0 +1,9 @@
+let numOfDrumBtn = document.querySelectorAll(".drum").length;
+for(let i = 0; i < numOfDrumBtn; i++) {
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+  alert("I got clicked!");
+  
+  //What to do when click detected.
+});
+}
+
