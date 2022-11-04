0465b33c2306cee5ae3c8f211b9066612d10efd9
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-04 09:52:19 +0000

    Callback function

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
+//Detecting Button Press
+
 let numOfDrumBtn = document.querySelectorAll(".drum").length;
-for(let i = 0; i < numOfDrumBtn; i++) {
-document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+for (let i = 0; i < numOfDrumBtn; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let btnInnerHTML = this.innerHTML;
 
-let btnInnerHTML = this.innerHTML;
-switch (btnInnerHTML) {
-  case "w":
-    let tom1 = new Audio("sounds/tom-1.mp3");
-    tom1.play();
-    break;
-
-  case "a":
-    let tom2 = new Audio("sounds/tom-2.mp3");
-    tom2.play();
-    break;
-
-  case "s":
-    let tom3 = new Audio("sounds/tom-3.mp3");
-    tom3.play();
-    break;
-
-  case "d":
-    let tom4 = new Audio("sounds/tom-4.mp3");
-    tom4.play();
-    break;
-
-  case "j":
-    let snare = new Audio("sounds/snare.mp3");
-    snare.play();
-    break;
-
-  case "k":
-    let crash = new Audio("sounds/crash.mp3");
-    crash.play();
-    break;
-
-  case "l":
-    let kick = new Audio("sounds/kick.mp3");
-    kick.play();
-    break;
-
-  default: console.log(btnInnerHTML);
-    
-  
+    makeSound(btnInnerHTML);
+  });
 }
+
+//Dtecting Keyboard Press
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
 });
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("sounds/kick.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(btnInnerHTML);
+  }
 }
-// addEventListener("keypress", function(event){
-//     alert("Key was pressed!");});
